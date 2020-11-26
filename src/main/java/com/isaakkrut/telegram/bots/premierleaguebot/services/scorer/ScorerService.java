d809f768075440c1e33affa2e22512b1c36ebe09
Isaak <krutisaak099@gmail.com>
2020-11-25 19:56:21 -0500

    Implemented Scorer Service and added cache

---
+++ src/main/java/com/isaakkrut/telegram/bots/premierleaguebot/services/scorer/ScorerService.java
@@ -0,0 +1,10 @@
+package com.isaakkrut.telegram.bots.premierleaguebot.services.scorer;
+
+import com.isaakkrut.telegram.bots.premierleaguebot.domain.Scorer;
+
+import java.util.List;
+
+public interface ScorerService {
+
+    List<Scorer> getAllScorers();
+}
---
+++ src/main/java/com/isaakkrut/telegram/bots/premierleaguebot/services/scorer/ScorerServiceJpa.java
@@ -0,0 +1,26 @@
+package com.isaakkrut.telegram.bots.premierleaguebot.services.scorer;
+
+import com.isaakkrut.telegram.bots.premierleaguebot.domain.Scorer;
+import com.isaakkrut.telegram.bots.premierleaguebot.repositories.ScorerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class ScorerServiceJpa implements ScorerService {
+    private final ScorerRepository scorerRepository;
+
+    @Cacheable(
+            value = "scorersCache")
+    @Override
+    public List<Scorer> getAllScorers() {
+        List<Scorer> scorers = new ArrayList<>();
+        scorerRepository.findAll().forEach(scorers::add);
+        return scorers;
+    }
+}
